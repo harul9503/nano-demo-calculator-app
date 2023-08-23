04d38ada88ac3ef00a7826d90c0c7fc1d703efab
harul9503 <harulganesh9503@gmail.com>
2023-08-23 23:38:13 +0530

    ji

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ from dataclasses import dataclass
 app = Flask(__name__)
 @dataclass
 class res:
-    res:int
+    result:int
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
@@ -12,15 +12,15 @@ def greeting():
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    n1=int(request.json['first'])
-    n2=int(request.json['second'])
-    return jsonify(result(n1+n2))
+    n1=int(request.json['f'])
+    n2=int(request.json['s'])
+    return jsonify(res(n1+n2))
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
     n1=int(request.json['first'])
     n2=int(request.json['second'])
-    return jsonify(result(n1-n2))
+    return jsonify(res(n1-n2))
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
